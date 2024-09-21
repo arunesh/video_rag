c374b90fd721dc7648ee9f3174d87dcae30bf228
Arunesh Mishra <aruneshm@gmail.com>
2024-09-21 16:27:50 -0700

    gitignore

--- video_rag.py
+++ video_rag.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title('Video RAG Demo')
+
+
+with st.form("Video Input:"):
+  st.write("Provide a YouTube URL or upload a video:")
+  my_number = st.slider('Pick a number', 1, 10)
+  my_color = st.selectbox('Pick a color', ['red','orange','green','blue','violet'])
+  st.form_submit_button('Submit')
+
